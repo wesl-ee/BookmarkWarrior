062a2889eeb8ea3dbd95a365ce71b676d8737dac
Wesley Coakley <w@wesleycoakley.com>
2020-02-20 17:13:30 -0500

    Basic installer

--- doc/scripts/install.sh
+++ doc/scripts/install.sh
@@ -2,11 +2,20 @@
 
 PKGDIR=$(dirname "$0")/../../
 
+INITDIR=/etc/init.d
 INSTDIR=/usr/local/bin
 DATADIR=/usr/local/share/BookmarkWarrior
 
 mkdir -p "$DATADIR"
+echo "Copying init script to $INITDIR"
+cp "$PKGDIR/doc/scripts/openrc-bookmarkwarrior.sh" "$INITDIR/bookmarkwarrior"
+
+echo "Copying binary to $INSTDIR"
 cp "$PKGDIR/BookmarkWarrior" "$INSTDIR/"
+
+echo "Copying data to $DATADIR"
 cp -r "$PKGDIR/tmpl" "$DATADIR/"
 cp -r "$PKGDIR/static" "$DATADIR/"
 cp "$PKGDIR/Config.toml" "$DATADIR/"
+
+echo "Done!"
